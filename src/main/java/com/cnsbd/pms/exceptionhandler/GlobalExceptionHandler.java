2f121aef200865ce10396d037105bc8358763e5a
Md Zahidul Islam Sumon <zsumon.cns@gmail.com>
2024-07-30 09:41:31 +0600

    GlobalExceptionHandler added, bad credentials Exception handled

---
+++ src/main/java/com/cnsbd/pms/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.cnsbd.pms.exceptionhandler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationException(AuthenticationException e) {
+        log.info("AuthenticationException: {}", e.getMessage(), e);
+        return ResponseEntity.status(HttpStatusCode.valueOf(401))
+                .body("Invalid username or password");
+    }
+}
