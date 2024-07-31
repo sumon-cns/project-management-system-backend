3ed0485aae792e9e04dc84a98f7f9dcd911f327e
Md Zahidul Islam Sumon <zsumon.cns@gmail.com>
2024-07-31 15:14:28 +0600

    PmUserRepository updated

--- src/main/java/com/cnsbd/pms/pmuser/PmUserRepository.java
+++ src/main/java/com/cnsbd/pms/pmuser/PmUserRepository.java
@@ -27,4 +27,24 @@ public interface PmUserRepository extends JpaRepository<PmUser, Integer> {
                             """,
             nativeQuery = true)
     List<PmUser> findAvailableUsers(@Param("projectId") Integer projectId);
+
+    /*
+     * this query is provided by chatgpt*/
+    @Query(
+            value =
+                    """
+                            SELECT u.*
+                            FROM pm_users u
+                                     LEFT JOIN (SELECT DISTINCT pu.pm_user_id
+                                                FROM projects p
+                                                         LEFT JOIN project_pm_user pu ON p.id = pu.project_id
+                                                WHERE p.id = :projectId) excluded_users ON u.id = excluded_users.pm_user_id
+                                     LEFT JOIN (SELECT proj.owner_id
+                                                FROM projects proj
+                                                WHERE proj.id = :projectId) excluded_owners ON u.id = excluded_owners.owner_id
+                            WHERE excluded_users.pm_user_id IS NULL
+                              AND excluded_owners.owner_id IS NULL
+                            """,
+            nativeQuery = true)
+    List<PmUser> findAvailableUsersV1(@Param("projectId") Integer projectId);
 }
