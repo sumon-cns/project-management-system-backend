d66aadb2bf552ff5bb6fde59d5a3a4d4779b6a6a
Md Zahidul Islam Sumon <zsumon.cns@gmail.com>
2024-08-03 16:33:03 +0600

    Handled ExpiredJwtException properly

--- src/main/java/com/cnsbd/pms/security/JwtRequestFilter.java
+++ src/main/java/com/cnsbd/pms/security/JwtRequestFilter.java
@@ -3,6 +3,7 @@ package com.cnsbd.pms.security;
 import com.cnsbd.pms.pmuser.PmUserDetailsService;
 import com.cnsbd.pms.util.JwtUtil;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -37,7 +38,13 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
-            username = jwtUtil.extractUsername(jwt);
+            try {
+                username = jwtUtil.extractUsername(jwt);
+            } catch (ExpiredJwtException ex) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write(ex.getMessage());
+                return;
+            }
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
